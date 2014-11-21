9e876d6385a0e4a40feb6ac850b1d97174ee59b3
mizoki <h.mizoki@gmail.com>
2014-11-22 03:52:08 +0900

    Add setup script of linuxbrew

---
+++ setup-linuxbrew.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# ref. http://brew.sh/linuxbrew/#installation
+
+init_linuxbrew_ubuntu() {
+  sudo aptitude update
+  sudo aptitude install \
+    build-essential \
+    curl \
+    git \
+    m4 \
+    ruby \
+    texinfo \
+    libbz2-dev \
+    libcurl4-openssl-dev \
+    libexpat-dev \
+    libncurses-dev \
+    zlib1g-dev
+}
+
+init_linuxbrew_centos() {
+  sudo yum update
+  sudo yum groupinstall 'Development Tools'
+  sudo yum install \
+    curl \
+    git \
+    m4 \
+    ruby \
+    texinfo \
+    bzip2-devel \
+    curl-devel \
+    expat-devel \
+    ncurses-devel \
+    zlib-devel
+}
+
+setup_linuxbrew() {
+  ruby -e "$(wget -O- https://raw.github.com/Homebrew/linuxbrew/go/install)"
+  # git clone https://github.com/Homebrew/linuxbrew.git ~/.linuxbrew
+
+  echo ''                                                      >> $HOME/.zshenv
+  echo '# Linuxbrew'                                           >> $HOME/.zshenv
+  echo 'export PATH=$HOME/.linuxbrew/bin:$PATH'                >> $HOME/.zshenv
+  echo 'export MANPATH=$HOME/.linuxbrew/share/man:$MANPATH'    >> $HOME/.zshenv
+  echo 'export INFOPATH=$HOME/.linuxbrew/share/info:$INFOPATH' >> $HOME/.zshenv
+  source $HOME/.zshenv
+}
