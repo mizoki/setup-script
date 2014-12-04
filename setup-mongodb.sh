c62234624f42baa0996f7552db4fd600d2487006
mizoki <h.mizoki@gmail.com>
2014-12-04 09:04:04 +0900

    Add setup script of mongodb for CentOS

--- setup-mongodb.sh
+++ setup-mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-init_mongodb_ubuntu() {
+setup_mongodb_ubuntu() {
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
   echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
@@ -11,4 +11,21 @@ init_mongodb_ubuntu() {
   # ref. http://te2u.hatenablog.jp/entry/2013/02/06/222342
   echo "smallfiles = true" | sudo tee -a /etc/mongodb.conf
   sudo service mongod restart
+}
+
+setup_mongodb_centos() {
+  echo ''                                                                   | sudo tee -a /etc/yum.repos.d/CentOS-Base.repo
+  echo '[10gen]'                                                            | sudo tee -a /etc/yum.repos.d/CentOS-Base.repo
+  echo 'name=10gen Repository'                                              | sudo tee -a /etc/yum.repos.d/CentOS-Base.repo
+  echo 'baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/' | sudo tee -a /etc/yum.repos.d/CentOS-Base.repo
+  echo 'gpgcheck=0'                                                         | sudo tee -a /etc/yum.repos.d/CentOS-Base.repo
+  echo 'enabled=0'                                                          | sudo tee -a /etc/yum.repos.d/CentOS-Base.repo
+
+  sudo yum -y --enablerepo=10gen install mongo-10gen mongo-10gen-server
+
+  # ref. http://te2u.hatenablog.jp/entry/2013/02/06/222342
+  echo "smallfiles = true" | sudo tee -a /etc/mongodb.conf
+
+  sudo /sbin/service mongod start
+  sudo /sbin/chkconfig mongod on
 }
