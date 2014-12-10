0c5a0afdc8a4c359307547c8b0617035e862c0e0
mizoki <h.mizoki@gmail.com>
2014-12-10 09:56:53 +0900

    Add setup script of tmuxinator

---
+++ setup-tmuxinator.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+setup_tmuxinator() {
+  gem install tmuxinator
+
+  # __________________________________________________________
+  # ..........................................................
+
+  # Thank you for installing tmuxinator.
+
+  # Make sure that you've set these variables in your ENV:
+
+  # $EDITOR, $SHELL
+
+  # You can run `tmuxinator doctor` to make sure everything is set.
+  # Happy tmuxing with tmuxinator!
+
+  # ..........................................................
+  # __________________________________________________________
+}
